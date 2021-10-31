04d69ef0cba452db3200ad38de14364b1f4be28b
RaminImani <Raminimani01@gmail.com>
2021-10-31 16:41:10 -0400

    tracking the ArrayIntro file

---
+++ src/com/syntax/class2/ArrayIntro.java
@@ -0,0 +1,22 @@
+package com.syntax.class2;
+
+public class ArrayIntro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char [] grades=new char[6];
+		
+		grades[0]='A';
+		grades[1]='B';
+		grades[2]='C';
+		grades[3]='D';
+		grades[4]='E';
+		grades[5]='F';
+		
+		System.out.println(grades[1]);
+		
+		
+	}
+
+}
